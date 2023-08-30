b318a1b92bcf2311e2cebb4cdbc308f1ec60801e
Josh DeCristi <joshdecristi@Joshs-MacBook-Pro.local>
2023-08-30 19:31:23 -0400

    feat: initial commit

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,19 @@
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+import { env } from "@src/env.mjs";
+import { appRouter } from "@src/server/api/root";
+import { createTRPCContext } from "@src/server/api/trpc";
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
+  onError:
+    env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+          );
+        }
+      : undefined,
+});
